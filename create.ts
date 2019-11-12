7192bcfde73bdc0293ee89e4ba72fcc2d897408c
Dennis Seidel <den.seidel@gmail.com>
2019-11-12 21:42:10 +0100

    feat: remove comments

--- create.ts
+++ create.ts
@@ -5,18 +5,9 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const main: APIGatewayProxyHandler = async (event, _context) => {
   try {
-    // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body)
     const params = {
       TableName: process.env.tableName,
-      // 'Item' contains the attributes of the item to be created
-      // - 'userId': user identities are federated through the
-      //             Cognito Identity Pool, we will use the identity id
-      //             as the user id of the authenticated user
-      // - 'noteId': a unique uuid
-      // - 'content': parsed from request body
-      // - 'attachment': parsed from request body
-      // - 'createdAt': current Unix timestamp
       Item: {
         userId: event.requestContext.identity.cognitoIdentityId,
         noteId: uuid.v1(),
@@ -26,7 +17,6 @@ export const main: APIGatewayProxyHandler = async (event, _context) => {
       }
     }
     await dynamoDbLib.call("put", params)
-    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
     return success(params.Item)
   } catch (e) {
     return failure({ status: false })
