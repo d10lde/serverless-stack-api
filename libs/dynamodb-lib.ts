c181a0aede9748a741007ff45c30058c78d986a7
Dennis Seidel <den.seidel@gmail.com>
2019-11-13 09:08:41 +0100

    feat: list notes

--- libs/dynamodb-lib.ts
+++ libs/dynamodb-lib.ts
@@ -2,7 +2,7 @@ import * as AWS from "aws-sdk";
 
 // TODO: make parameter able to be used with all dynamodb Input types (Udemy Typescript course) 
 // without extending the function definition all the time
-export function call(action: string, params: AWS.DynamoDB.DocumentClient.PutItemInput | AWS.DynamoDB.DocumentClient.GetItemInput) {
+export function call(action: string, params: AWS.DynamoDB.DocumentClient.PutItemInput | AWS.DynamoDB.DocumentClient.GetItemInput | AWS.DynamoDB.DocumentClient.QueryInput) {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   return dynamoDb[action](params).promise();
